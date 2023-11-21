7ba6d27ef5ecf2fa94fab906309e621a9ec8d83a
carolina <ci.arayagonzalez@gmail.com>
2023-11-21 20:59:55 -0300

    add gameboard

---
+++ app.js
@@ -0,0 +1,36 @@
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
+const optionContainer = document.querySelector('.option-container')
+const flipButton = document.querySelector('#flip-button')
+
+//Option choosing
+let angle = 0
+function flip() {
+    const optionShips = Array.from(optionContainer.children)
+    // if (angle === 0) {
+    //     angle = 90
+    // } else {
+    //     angle = 0
+    // }
+    angle = angle === 0 ? 90 : 0
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
+}
+
+//creating Boards
+const width = 10
+
+function createBoard(){
+    const gameBoardContainer = document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor = 'pink'
+
+    gamesBoardContainer.append(gameBoardContainer)
+
+}
+
+
+
+
+
+
+
+flipButton.addEventListener('click', flip)
