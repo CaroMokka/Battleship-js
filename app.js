640f99dfb6e9eff6997fab4dd3c7cc823a0a8cb6
carolina <ci.arayagonzalez@gmail.com>
2023-11-22 13:08:30 -0300

    :sparkles: Add logic ships aleatory computer

--- app.js
+++ app.js
@@ -14,23 +14,59 @@ function flip() {
     angle = angle === 0 ? 90 : 0
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
+flipButton.addEventListener('click', flip)
 
 //creating Boards
 const width = 10
 
-function createBoard(){
+function createBoard(color, user){
     const gameBoardContainer = document.createElement('div')
     gameBoardContainer.classList.add('game-board')
-    gameBoardContainer.style.backgroundColor = 'pink'
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
 
     gamesBoardContainer.append(gameBoardContainer)
 
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
 }
 
+createBoard('yellow', 'player')
+createBoard('pink', 'computer')
 
+//creating ships
+class Ship {
+    constructor(name, length) {
+        this.name = name
+        this.length = length
+    }
+}
+const destroyer = new Ship ('destroyer', 2)
+const submarine = new Ship ('submarine', 3)
+const cruiser = new Ship ('cruiser', 3)
+const battleship = new Ship ('battleship', 4)
+const carrier = new Ship ('carrier', 5)
 
+const ships = [destroyer, submarine, cruiser, battleship, carrier]
+console.log(ships)
 
+function addShipPiece(ship) {
+    const allBoardBlocks = document.querySelectorAll('#computer div')//obtengo el tablero de divs de computer
+    const randomBoolean = Math.random() > 0.5
+    const isHorizontal = true
+    const randomStarIndex = Math.floor(Math.random() * width * width)
+    console.log(randomStarIndex)
 
+    for (let i = 0; i < ship.length; i++) {
+        if(isHorizontal){
+            console.log(allBoardBlocks[Number(randomStarIndex) + i])
+        }
+    }
 
+}
 
-flipButton.addEventListener('click', flip)
+addShipPiece(destroyer)
